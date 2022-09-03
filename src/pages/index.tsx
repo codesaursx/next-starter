471a0418331e85b2404275845d249b0d7ea4594a
Angel Martinez <angel.martineztrc@gmail.com>
2022-09-03 16:32:33 -0500

    feat:  add eslint and prettier config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,8 @@ import type { NextPage } from 'next';
 
 const HomePage: NextPage = () => {
   return (
-    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
-      <h1 className="text-6xl font-bold font-mono text-white">
+    <div className="flex min-h-screen items-center justify-center bg-gray-900">
+      <h1 className="font-mono text-6xl font-bold text-white">
         CodesaursX<span className="text-indigo-500">.</span>
       </h1>
     </div>
