ba5e1de7932052b5efdb90ab3d7b67b0b8d2bba7
Angel Martinez <angel.martineztrc@gmail.com>
2022-09-03 16:23:24 -0500

    feat:  add tailwindcss to the project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,10 @@ import type { NextPage } from 'next';
 
 const HomePage: NextPage = () => {
   return (
-    <div>
-      <h1>Hello, World!</h1>
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+      <h1 className="text-6xl font-bold font-mono text-white">
+        CodesaursX<span className="text-indigo-500">.</span>
+      </h1>
     </div>
   );
 };
